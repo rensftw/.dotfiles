b7e69190fbbb7058e6ff725f714c98932121d716
Irena Angelova <rensftw@users.noreply.github.com>
2021-04-17 22:42:23 +0300

    Fix homebrew installation script

--- _scripts/install-homebrew.sh
+++ _scripts/install-homebrew.sh
@@ -1,6 +1,6 @@
 if ! command -v brew &> /dev/null; then
     echo "🍺 ${CYAN}Installing Homebrew${NC}"
-    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     exit
 else
     echo "☑️  ${GREEN}Homebrew has already been installed${NC}"
