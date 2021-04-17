90418d9d3e793fd98b87135b0518d7ddcb553f44
Irena Angelova <rensftw@users.noreply.github.com>
2021-04-17 23:23:53 +0300

    Update uninstall script

---
+++ _scripts/unstow.sh
@@ -0,0 +1,14 @@
+# Helper directories begin with _ (e.g. _scripts)
+HELPER_DIR_PREFIX='_*'
+
+# Store all directory names in an array
+directories=($(ls -d */))
+
+# Ignore helper directories when unstowing
+for dir in "${directories[@]}"; do
+    if [[ $dir != $HELPER_DIR_PREFIX ]]; then
+        echo "🔗 Unlinking ${PURPLE}${dir%/}${NC}"
+        stow -Dt ~ $dir
+    fi
+done
+
