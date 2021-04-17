c93f0f77d82b6ab1bbbe53cba7e6d10a35b5eb29
Irena Angelova <rensftw@users.noreply.github.com>
2021-04-17 23:36:26 +0300

    Potential fix for the brew bundle subcommand issue

--- _scripts/install-homebrew.sh
+++ _scripts/install-homebrew.sh
@@ -1,6 +1,9 @@
 if ! command -v brew &> /dev/null; then
     echo "🍺 ${CYAN}Installing Homebrew${NC}"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    #Update to the latest brew version (might help with the bundle subcommand issue?)
+    brew update
     exit
 else
     echo "☑️  ${GREEN}Homebrew has already been installed${NC}"
