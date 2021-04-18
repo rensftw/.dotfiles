94d59d662bd53222a4050a5f511ef8a2adba8283
Irena Angelova <rensftw@users.noreply.github.com>
2021-04-18 15:38:37 +0300

    Tap homebrew/bundle before trying to use it

--- _scripts/install-from-brewfile.sh
+++ _scripts/install-from-brewfile.sh
@@ -1,4 +1,7 @@
+echo "🚰 ${CYAN}Tapping homebrew/bundle${NC}"
+# Get Homebrew/bundle before trying to use it
+brew tap homebrew/bundle
+
 # Install all taps, formulae, and casks from the Brewfile
 echo "📦 ${CYAN}Installing Homebrew packages${NC}"
-
 brew bundle install --all --cleanup --file _homebrew/Brewfile
