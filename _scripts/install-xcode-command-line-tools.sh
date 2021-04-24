6db92b3b1778cd5baf9b1ba648ffd460a4dc2294
Irena Angelova <rensftw@users.noreply.github.com>
2021-04-24 17:04:33 +0300

    Remove script for installing XCode CommandLineTools, because Homebrew already does that automatically

--- _scripts/install-xcode-command-line-tools.sh
+++
@@ -1,8 +0,0 @@
-if ! command -v xcode-select --version &> /dev/null; then
-    echo "🛠 ${CYAN}Installing XCode command line tools${NC}"
-    xcode-select --install
-    exit
-else
-    echo "☑️  ${GREEN}Xcode command line tools already installed${NC}"
-    exit
-fi
