43cf0f600cb359add275b8700fc8da526443bd8b
Irena <rensftw@users.noreply.github.com>
2021-04-26 21:28:16 +0300

    Improve comments and fix variable name

--- _scripts/stow.sh
+++ _scripts/stow.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-
 echo "🧹 ${CYAN}Clean default zsh artifacts${NC}"
 rm -rf $HOME/.zshrc $HOME/.zshenv $HOME/.zsh $HOME/.zsh_plugins &> /dev/null
 
-# Helper directories begin with _ (e.g. _scripts)
+# Helper directories begin with an underscore (e.g. _scripts)
 HELPER_DIR_PREFIX='_*'
 
 # Store all directory names in an array
-directories=($(ls -d */))
+DIRECTORIES=($(ls -d */))
 
 # Ignore helper directories when stowing
-for dir in "${directories[@]}"; do
+for dir in "${DIRECTORIES[@]}"; do
     if [[ $dir != $HELPER_DIR_PREFIX ]]; then
         echo "🔗 Linking ${PURPLE}${dir%/}${NC}"
         stow -vt ~ $dir
