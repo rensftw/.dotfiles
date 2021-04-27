ce84d5c0187a9c35774bf4d690a7234c6724d331
Irena Angelova <rensftw@users.noreply.github.com>
2021-04-27 14:50:41 +0300

    Add custom mkcd command

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -7,7 +7,7 @@
 # A simple command for demonstration purposes follows.
 # -----------------------------------------------------------------------------
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
 # You can import any python module as needed.
 import os
@@ -60,3 +60,38 @@ class my_edit(Command):
         # This is a generic tab-completion function that iterates through the
         # content of the current directory.
         return self._tab_directory_content()
+
+
+class mkcd(Command):
+    """
+    :mkcd <dirname>
+
+    Creates a directory with the name <dirname> and enters it.
+    """
+
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        import re
+
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+
+            match = re.search("^/|^~[^/]*/", dirname)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0) :]
+
+            for m in re.finditer("[^/]+", dirname):
+                s = m.group(0)
+                if s == ".." or (
+                    s.startswith(".") and not self.fm.settings["show_hidden"]
+                ):
+                    self.fm.cd(s)
+                else:
+                    ## We force ranger to load content before calling `scout`.
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console("scout -ae ^{}$".format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
