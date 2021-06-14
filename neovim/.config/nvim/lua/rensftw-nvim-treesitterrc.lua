e3f2da51a8c3c211b08401c15002cfa478fa301e
Irena <rensftw@users.noreply.github.com>
2021-06-14 14:38:34 +0300

    Add treesitter config

---
+++ neovim/.config/nvim/lua/rensftw-nvim-treesitterrc.lua
@@ -0,0 +1,6 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "maintained", -- install all parsers with maintainers
+    highlight = { enable = true },
+    indent = { enable = true },
+}
+
