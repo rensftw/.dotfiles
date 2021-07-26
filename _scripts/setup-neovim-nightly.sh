558b5f7a3823d8a10f618c1176d53fe818fdd6bb
Irena <rensftw@users.noreply.github.com>
2021-07-26 11:53:13 +0300

    Clean dist folde before trying to build neovim

--- _scripts/setup-neovim-nightly.sh
+++ _scripts/setup-neovim-nightly.sh
@@ -2,6 +2,9 @@ echo "🌙 ${CYAN}Setting up neovim-nightly${NC}"
 
 NEOVIM_NIGHTLY_DIR = "$HOME/.dotfiles/neovim/neovim-nightly"
 
+# Clean old build (if there is one)
+make distclean
+
 # Build Neovim
 make CMAKE_BUILD_TYPE=Release
 
