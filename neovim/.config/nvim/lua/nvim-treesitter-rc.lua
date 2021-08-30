1c4ff9bfa0faa53f20f2931c335f82494a57faac
Irena <rensftw@users.noreply.github.com>
2021-08-30 16:11:29 +0300

    Uncomment problematic config because it seems like nvim-treesitter's bug has been fixed

--- neovim/.config/nvim/lua/nvim-treesitter-rc.lua
+++ neovim/.config/nvim/lua/nvim-treesitter-rc.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    -- ensure_installed = "maintained", -- install all parsers with maintainers
+    ensure_installed = "maintained", -- install all parsers with maintainers
     highlight = { enable = true },
     indent = { enable = true },
 }
