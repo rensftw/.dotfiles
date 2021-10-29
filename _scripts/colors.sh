6a04fcaf9ac989d6f68fab75590ccc4656e110d6
Irena <rensftw@users.noreply.github.com>
2021-10-29 17:10:53 +0300

    Add more color variables

--- _scripts/colors.sh
+++ _scripts/colors.sh
@@ -3,4 +3,6 @@ export RED='\033[1;31m'
 export PURPLE='\033[1;35m'
 export CYAN='\033[1;36m'
 export GREEN='\033[1;32m'
+export YELLOW_BLINK='\033[33;5;7m'
+export BLINK='\033[5m'
 export NC='\033[0m' # No color
