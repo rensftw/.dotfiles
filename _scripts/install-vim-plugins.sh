b31578b3e5165d18251cbb15b90916096ec73094
Irena Angelova <rensftw@users.noreply.github.com>
2021-10-30 16:59:02 +0300

    ! makes sure all Plug runs all steps unconditionally (fetch, pull, run post-update hooks, etc)

--- _scripts/install-vim-plugins.sh
+++ _scripts/install-vim-plugins.sh
@@ -1,4 +1,4 @@
 echo "🔌 ${CYAN}Installing vim plugins${NC}"
 
 # We need to run PlugUpdate! manually, because there is an issue with VimPlug's post-install hook
-nvim --headless +'PlugInstall --sync' +'PlugUpdate!' +qall
+nvim --headless +'PlugInstall! --sync' +'PlugUpdate!' +qall
