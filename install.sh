4359fd9e9f7ac7e9b2cd2184e336fbd2d483569c
Irena <rensftw@users.noreply.github.com>
2021-10-30 19:31:15 +0300

    Add reminder

--- install.sh
+++ install.sh
@@ -3,21 +3,33 @@
 # Import ANSI escape codes for colors
 source _scripts/colors.sh
 
-echo "🏁 ${PURPLE}Beginning installation...${NC}"
+echo "This script requires Terminal.app to have full disk access privileges.\nTo continue grant those permissions via Preferences > Security & Privacy > Privacy > Full Disk Access"
+echo "${YELLOW_BLINK}Proceed? (y/n)${NC}"
+read ANSWER
 
-# Ask for sudo and maintain it until all steps are complete
-sh _scripts/ask-for-admin.sh
 
-# Install Homebrew
-sh _scripts/install-homebrew.sh
+if [[ "$ANSWER" == "y" || "$ANSWER" == "yes" ]]; then
 
-# Install all the packages defined in Brewfile (taps, formulae, casks, and MAS apps)
-sh _scripts/install-from-brewfile.sh
+    echo "🏁 ${PURPLE}Beginning installation...${NC}"
 
-echo "🎉 ${GREEN}Installation complete!${NC}"
-sh _scripts/goodbye.sh
+    # Ask for sudo and maintain it until all steps are complete
+    sh _scripts/ask-for-admin.sh
 
-# Restart the session
-if [[ $SHELL =~ 'zsh' ]]; then
-    exec zsh &> /dev/null
+    # Install Homebrew
+    sh _scripts/install-homebrew.sh
+
+    # Install all the packages defined in Brewfile (taps, formulae, casks, and MAS apps)
+    sh _scripts/install-from-brewfile.sh
+
+    echo "🎉 ${GREEN}Installation complete!${NC}"
+    sh _scripts/goodbye.sh
+
+    # Restart the session
+    if [[ $SHELL =~ 'zsh' ]]; then
+        exec zsh &> /dev/null
+    fi
+else
+    echo "${CYAN}No changes made. Quitting.."${NC}
+    exit
 fi
+
