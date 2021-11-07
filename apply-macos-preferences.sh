949a289f3364777c3925587476a7084dedd16f8f
Irena <rensftw@users.noreply.github.com>
2021-11-07 20:00:00 +0200

    Do not show status bar

--- apply-macos-preferences.sh
+++ apply-macos-preferences.sh
@@ -114,9 +114,6 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 # Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show status bar
-defaults write com.apple.finder ShowStatusBar -bool true
-
 # Finder: show path bar
 defaults write com.apple.finder ShowPathbar -bool true
 
