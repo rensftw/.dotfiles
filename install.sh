6499c1db23b101e78649d85e016a6c2fa4bd5427
Irena Angelova <rensftw@users.noreply.github.com>
2021-11-07 18:53:01 +0200

    Remove install.sh prompt - there is no need for Terminal.app to have full disk access to run the scripts

--- install.sh
+++ install.sh
@@ -3,38 +3,25 @@
 # Import ANSI escape codes for colors
 source _scripts/colors.sh
 
-echo "This script requires Terminal.app to have full disk access privileges.
-To continue grant those permissions via ${PURPLE}Preferences > Security & Privacy > Privacy > Full Disk Access${NC}
+echo "🏁 ${PURPLE}Beginning installation...${NC}"
 
-"
+# Install Homebrew
+sh _scripts/install-homebrew.sh
 
-echo "${YELLOW_BLINK}Proceed? (y/n)${NC}"
-read ANSWER
+# Install all packages defined in Brewfile (taps, formulae, casks, and MAS apps)
+sh _scripts/install-from-brewfile.sh
 
-if [[ "$ANSWER" == "y" || "$ANSWER" == "yes" ]]; then
-    echo "🏁 ${PURPLE}Beginning installation...${NC}"
+# Install Node version manager (nvm)
+sh _scripts/install-nvm.sh
 
-    # Install Homebrew
-    sh _scripts/install-homebrew.sh
+# Install the current LTS Node version with the default global packages
+sh _scripts/install-lts-node.sh
 
-    # Install all packages defined in Brewfile (taps, formulae, casks, and MAS apps)
-    sh _scripts/install-from-brewfile.sh
+# Install Python packages
+sh _scripts/install-pip-packages.sh
 
-    # Install Node version manager (nvm)
-    sh _scripts/install-nvm.sh
+echo "🎉 ${GREEN}Installation complete!${NC}"
+sh _scripts/goodbye.sh
 
-    # Install the current LTS Node version with the default global packages
-    sh _scripts/install-lts-node.sh
-
-    # Install Python packages
-    sh _scripts/install-pip-packages.sh
-
-    echo "🎉 ${GREEN}Installation complete!${NC}"
-    sh _scripts/goodbye.sh
-
-    exit
-else
-    echo "${CYAN}No changes made. Quitting.."${NC}
-    exit
-fi
+exit
 
