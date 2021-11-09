755310fb820436dd757d3b95f78c7c403dff4e16
Irena <rensftw@users.noreply.github.com>
2021-11-09 14:29:56 +0200

    Add Brew prefixes, global variables, and update PATH for Apple Silicon machines. Homebrew's preferred prefix is /usr/local for Intel macs and /opt/homebrew for Apple Silicon macs

--- _scripts/install-homebrew.sh
+++ _scripts/install-homebrew.sh
@@ -1,8 +1,15 @@
+ARCH=$(arch)
+
 if command -v brew &> /dev/null; then
     echo "☑️  ${GREEN}Homebrew has already been installed${NC}"
 else
     echo "🍺 ${CYAN}Installing Homebrew${NC}"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Apple Silicon macs do not automatically include Brew in the global PATH
+    if [[ $ARCH =~ 'arm' ]]; then
+        eval $(/opt/homebrew/bin/brew shellenv)
+    fi
 
     # Disable Homebrew analytics (which are on by default)
     brew analytics off
