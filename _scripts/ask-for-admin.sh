aaa5ef44e5ed78dd10de832f952ae74e4e1ee660
Irena Angelova <rensftw@users.noreply.github.com>
2021-11-10 22:58:35 +0200

    scripts: Remove ask-for-admin.sh since it's unused

--- _scripts/ask-for-admin.sh
+++
@@ -1,7 +0,0 @@
-echo "🔐 ${CYAN}Checking for sudo (might require password)${NC}" 
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
