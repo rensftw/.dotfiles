b5dd7d1569a73e3803b94ec53616907f235aa3fa
Irena <rensftw@users.noreply.github.com>
2021-12-18 22:26:09 +0200

    neovim(lsp): Remove option for omnifunc completion, since we rely on nvim-cmp

--- neovim/.config/nvim/lua/lspconfig-rc.lua
+++ neovim/.config/nvim/lua/lspconfig-rc.lua
@@ -18,12 +18,8 @@ end
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
+  -- Mappings
   local opts = { noremap=true, silent=true }
 
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
