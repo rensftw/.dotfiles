6ebdc778f5c9fcc11fc11f2d783f642f789495ea
Irena <rensftw@users.noreply.github.com>
2021-12-19 18:29:10 +0200

    neovim: Correct alpha telescope commands

--- neovim/.config/nvim/lua/alpha-rc.lua
+++ neovim/.config/nvim/lua/alpha-rc.lua
@@ -16,8 +16,8 @@ dashboard.section.header.val = {
 -- Set menu
 dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "f", "  > Find file", ":Telescope find_files previewer=false<CR>"),
+    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles previewer=false<CR>"),
     dashboard.button( "s", "  > Settings" , ":e $VIMRC_LOCATION | :cd %:p:h | split . | wincmd k | pwd<CR>"),
     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
