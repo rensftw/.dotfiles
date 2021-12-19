d1d728eab97a6062a14fc6b93f3b826021c70a15
Irena <rensftw@users.noreply.github.com>
2021-12-19 17:29:43 +0200

    neovim(refactor): Migrate from VimPlug to packer.nvim

--- _scripts/install-neovim-plugins.sh
+++ _scripts/install-neovim-plugins.sh
@@ -1,4 +1,3 @@
 echo "🔌 ${CYAN}Installing vim plugins${NC}"
 
-# We need to run PlugUpdate! manually, because there is an issue with VimPlug's post-install hook
-nvim --headless +'PlugInstall! --sync' +'PlugUpdate!' +qall > /dev/null
+nvim --headless +'PackerInstall' +qall > /dev/null
