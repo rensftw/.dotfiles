8385679613bd6045f6b65fcb21c290963686a433
Irena <rensftw@users.noreply.github.com>
2021-12-22 21:24:46 +0200

    neovim: Add keymap for triggering autocompletion

--- neovim/.config/nvim/lua/plugin/nvim-cmp-rc.lua
+++ neovim/.config/nvim/lua/plugin/nvim-cmp-rc.lua
@@ -9,12 +9,13 @@ cmp.setup({
     mapping = {
         ['<S-up>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<S-down>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-a>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
         ['<C-y>'] = cmp.config.disable,
         ['<C-e>'] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
