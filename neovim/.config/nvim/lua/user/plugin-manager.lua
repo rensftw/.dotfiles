a6fad0d062c7d74fdb6c5c535d1476e8337519e8
Irena <rensftw@users.noreply.github.com>
2021-12-24 00:40:09 +0200

    neovim: Add command mode completion back. After enabling tab/s-tab navigation for cmp + ultisnips autocompletion in command mode stopped working.:(

--- neovim/.config/nvim/lua/user/plugin-manager.lua
+++ neovim/.config/nvim/lua/user/plugin-manager.lua
@@ -36,9 +36,10 @@ return require('packer').startup(function(use)
     use 'hrsh7th/nvim-cmp'
     -- completion sources:
     use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-nvim-lua'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-cmdline'
 
     -- JSON schemas
     use "b0o/schemastore.nvim"
