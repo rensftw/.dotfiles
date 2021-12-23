28d6282e65e425304828b89a56285b678ded7789
Irena <rensftw@users.noreply.github.com>
2021-12-23 23:52:40 +0200

    neovim(cmp, ultisnips): Allow for tab/s-tab to be used for autocompletion

--- neovim/.config/nvim/lua/plugin/ultisnips-rc.lua
+++ neovim/.config/nvim/lua/plugin/ultisnips-rc.lua
@@ -1,2 +1,5 @@
-vim.g.UltiSnipsJumpForwardTrigger = '<tab>'
-vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
+vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
+vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
+vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
+vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
+vim.g.UltiSnipsRemoveSelectModeMappings = 0
