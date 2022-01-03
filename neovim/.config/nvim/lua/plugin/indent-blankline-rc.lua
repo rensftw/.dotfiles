f6c85857ea614a20475b70c29736c36869c3dc5a
Irena <rensftw@users.noreply.github.com>
2022-01-03 16:46:19 +0200

    neovim: Add indentation based on treesitter syntax

---
+++ neovim/.config/nvim/lua/plugin/indent-blankline-rc.lua
@@ -0,0 +1,32 @@
+--[[
+-- Each treesitter grammar definers their own names for things.
+-- More info: https://github.com/lukas-reineke/indent-blankline.nvim/issues/271
+-- The following patterns cover JS/TS
+--]]
+vim.g.indent_blankline_context_patterns = {
+    'class',
+    'return',
+    'function',
+    'method',
+    '^if',
+    '^else',
+    '^while',
+    '^for',
+    '^object',
+    '^table',
+    'block',
+    'arguments',
+    'jsx_element',
+    'jsx_self_closing_element',
+    'try_statement',
+    'catch_clause',
+    'import_statement',
+    'operation_type'
+}
+
+require('indent_blankline').setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+    use_treesitter = true,
+}
