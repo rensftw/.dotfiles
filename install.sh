b30f6fa792700126369061130805ae6a9a682f07
Irena <22574186+rensftw@users.noreply.github.com>
2022-01-06 23:24:52 +0200

    scripts: Invoke scripts using source rather than sh

--- install.sh
+++ install.sh
@@ -6,25 +6,25 @@ source _scripts/colors.sh
 echo "🏁 ${PURPLE}Beginning installation...${NC}"
 
 # Install Homebrew
-sh _scripts/install-homebrew.sh
+source _scripts/install-homebrew.sh
 
 # Install all packages defined in Brewfile (taps, formulae, casks, and MAS apps)
-sh _scripts/install-from-brewfile.sh
+source _scripts/install-from-brewfile.sh
 
 # Install Node version manager (nvm)
-sh _scripts/install-nvm.sh
+source _scripts/install-nvm.sh
 
 # Install the current LTS Node version with the default global packages
-sh _scripts/install-lts-node.sh
+source _scripts/install-lts-node.sh
 
 # Install Python
-sh _scripts/install-python.sh
+source _scripts/install-python.sh
 
 # Install Python packages
-sh _scripts/install-pip-packages.sh
+source _scripts/install-pip-packages.sh
 
 echo "🎉 ${GREEN}Installation complete!${NC}"
-sh _scripts/goodbye.sh
+source _scripts/goodbye.sh
 
 exit
 
