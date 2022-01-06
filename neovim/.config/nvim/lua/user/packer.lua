9bfe87d8c18d3f4664acea667050033087eeb3ba
Irena <22574186+rensftw@users.noreply.github.com>
2022-01-07 00:10:17 +0200

    chore: Fix errors

--- neovim/.config/nvim/lua/user/packer.lua
+++ neovim/.config/nvim/lua/user/packer.lua
@@ -1,15 +1,15 @@
+---@diagnostic disable: different-requires
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 -- Automatically install Packer
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({
+    Packer_bootstrap = fn.system({
         'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
         install_path
     })
 end
 
----@diagnostic disable-next-line: different-requires
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
@@ -103,7 +103,7 @@ return require('packer').startup(function(use)
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
-    if packer_bootstrap then
+    if Packer_bootstrap then
         require('packer').sync()
     end
 end)
