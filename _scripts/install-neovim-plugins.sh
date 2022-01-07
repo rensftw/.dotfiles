a64fca4c78cc1a86535df76550c93f073f4e807c
Irena <22574186+rensftw@users.noreply.github.com>
2022-01-07 17:26:43 +0200

    chore: Add comment for future reference

--- _scripts/install-neovim-plugins.sh
+++ _scripts/install-neovim-plugins.sh
@@ -1,4 +1,7 @@
 echo "🔌 ${CYAN}Installing vim plugins${NC}"
 PACKER_CONFIG="$HOME/.dotfiles/neovim/.config/nvim/lua/user/packer.lua"
 
+# Load a bare minimum config (just the Packer portion) in order
+# to avoid errors from configs that depend on external plugins
+# (which are not installed yet, at this point)
 nvim -u $PACKER_CONFIG --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
