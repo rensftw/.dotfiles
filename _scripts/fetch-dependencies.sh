8ff49e3f8e65908858475d19ed590b3d8778ccbe
Irena <22574186+rensftw@users.noreply.github.com>
2022-01-30 01:51:57 +0200

    iterm, tmux, neovim: Switch workflow from iTerm to tmux

---
+++ _scripts/fetch-dependencies.sh
@@ -0,0 +1,16 @@
+# If .dotfiles is a git repo itself, add dependencies as git submodules
+if [[ -e .git ]]; then
+    echo "🛢  ${CYAN}Unpacking git submodules${NC}"
+
+    git submodule init
+    git submodule update
+else
+    # Otherwise, clone each dependency as a normal repo
+    echo "🚚 ${CYAN}Fetching dependencies${NC}"
+
+    DOTFILES=$(PWD)
+
+    echo "🧲 tmux package manager"
+    mkdir -p $DOTFILES/tmux/.tmux/tpm
+    git clone https://github.com/tmux-plugins/tpm.git $DOTFILES/tmux/.tmux/tpm
+fi
