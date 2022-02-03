ac089d65f3b98bd876c22e1baa023a942d8ded4a
Irena <22574186+rensftw@users.noreply.github.com>
2022-02-04 00:08:52 +0200

    neovim, tmux: Preserve tmux pane zoom and autosave neovim buffers when moving away from tmux pane

---
+++ neovim/.config/nvim/lua/plugin/vim-tmux-navigator-rc.lua
@@ -0,0 +1,5 @@
+-- Write all buffers before navigating from Vim to tmux pane
+vim.g.tmux_navigator_save_on_switch = 2
+
+-- If the tmux window is zoomed, keep it zoomed when moving from Vim to another pane
+vim.g.tmux_navigator_preserve_zoom = 1
