470cb4596bb8df455b0ef2576f9796e4d407c6d5
Irena Angelova <22574186+rensftw@users.noreply.github.com>
2022-03-11 15:17:27 +0200

    neovim(nvim-web-devicons): Show custom coloured icons for JS, TS, HTML, CSS, and git related artifacts

---
+++ neovim/.config/nvim/lua/plugin/nvim-web-devicons-rc.lua
@@ -0,0 +1,61 @@
+require'nvim-web-devicons'.setup {
+    override = {
+        ["js"] = {
+            icon = "",
+            color = "#f7dd4a",
+            cterm_color = "185",
+            name = "Js"
+        },
+        ["ts"] = {
+            icon = "ﯤ",
+            color = "#4477c0",
+            cterm_color = "67",
+            name = "Ts",
+        },
+        ["html"] = {
+            icon = "",
+            color = "#e34c26",
+            cterm_color = "166",
+            name = "Html",
+        },
+        ["css"] = {
+            icon = "",
+            color = "#3771b5",
+            cterm_color = "60",
+            name = "Css",
+        },
+        ["git"] = {
+            icon = "",
+            color = "#e45a38",
+            cterm_color = "59",
+            name = "GitIgnore",
+        },
+        [".gitattributes"] = {
+            icon = "",
+            color = "#e45a38",
+            cterm_color = "59",
+            name = "GitIgnore",
+        },
+        [".gitignore"] = {
+            icon = "",
+            color = "#e45a38",
+            cterm_color = "59",
+            name = "GitIgnore",
+        },
+        [".gitmodules"] = {
+            icon = "",
+            color = "#e45a38",
+            cterm_color = "59",
+            name = "GitIgnore",
+        },
+        ["COMMIT_EDITMSG"] = {
+            icon = "",
+            color = "#e45a38",
+            cterm_color = "59",
+            name = "GitIgnore",
+        },
+    };
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true;
+}
