55f9d859e5c6152bea3119a99caa1693ca3209ce
Irena <22574186+rensftw@users.noreply.github.com>
2022-03-21 18:47:06 +0200

    neovim(fidget.nvim): Increase fidget and task decay

--- neovim/.config/nvim/lua/plugin/fidget-rc.lua
+++ neovim/.config/nvim/lua/plugin/fidget-rc.lua
@@ -3,7 +3,7 @@ require('fidget').setup {
         spinner = 'dots',        -- animation shown when tasks are ongoing
     },
     timer = {
-        fidget_decay = 5000,     -- how long to keep around empty fidget, in ms
-        task_decay = 2000,       -- how long to keep around completed task, in ms
+        fidget_decay = 6000,     -- how long to keep around empty fidget, in ms
+        task_decay = 3000,       -- how long to keep around completed task, in ms
     }
 }
