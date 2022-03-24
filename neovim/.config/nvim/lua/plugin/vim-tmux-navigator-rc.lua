afc0db9db2a8be6d83693d684a517f2d0f5c892d
Irena <22574186+rensftw@users.noreply.github.com>
2022-03-24 14:42:27 +0200

    neovim, tmux: Disable tmux navigator when the Vim pane is zoomed

--- neovim/.config/nvim/lua/plugin/vim-tmux-navigator-rc.lua
+++ neovim/.config/nvim/lua/plugin/vim-tmux-navigator-rc.lua
@@ -1,5 +1,5 @@
 -- Write all buffers before navigating from Vim to tmux pane
 vim.g.tmux_navigator_save_on_switch = 2
 
--- If the tmux window is zoomed, keep it zoomed when moving from Vim to another pane
-vim.g.tmux_navigator_preserve_zoom = 1
+-- Disable tmux navigator when zooming the Vim pane
+vim.g.tmux_navigator_disable_when_zoomed = 1
