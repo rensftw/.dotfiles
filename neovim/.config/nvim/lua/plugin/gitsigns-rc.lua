99437a3654dd4e6565da102d5e5e502c21c112b9
Irena Angelova <22574186+rensftw@users.noreply.github.com>
2022-05-30 00:06:36 +0300

    neovim(keymaps): Reorganize and clean up git keymaps

--- neovim/.config/nvim/lua/plugin/gitsigns-rc.lua
+++ neovim/.config/nvim/lua/plugin/gitsigns-rc.lua
@@ -6,19 +6,5 @@ require('gitsigns').setup {
         topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
         changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
     },
-    keymaps = {
-        noremap = true,
-        ['n ]h'] = { expr = true, "&diff ? ']h' : '<cmd>Gitsigns next_hunk<CR>'" },
-        ['n [h'] = { expr = true, "&diff ? '[h' : '<cmd>Gitsigns prev_hunk<CR>'" },
-
-        ['n <leader>ha'] = '<cmd>Gitsigns stage_hunk<CR>',
-        ['v <leader>ha'] = ':Gitsigns stage_hunk<CR>',
-        ['n <leader>hu'] = '<cmd>Gitsigns reset_hunk<CR>',
-        ['v <leader>hu'] = ':Gitsigns reset_hunk<CR>',
-        ['n <leader>hp'] = '<cmd>Gitsigns preview_hunk<CR>',
-        ['n <leader>hA'] = '<cmd>Gitsigns stage_buffer<CR>',
-        ['n <leader>hU'] = '<cmd>Gitsigns reset_buffer<CR>',
-        ['n <leader>gB'] = '<cmd>Gitsigns toggle_current_line_blame<CR>',
-    },
     current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 }
