521f3b09ea1c103896a55f879714e08064040af7
Irena <22574186+rensftw@users.noreply.github.com>
2022-07-02 20:35:35 +0300

    scripts: Update installation link to new version of NVM

--- _scripts/install-nvm.sh
+++ _scripts/install-nvm.sh
@@ -2,5 +2,5 @@ if command -v nvm &> /dev/null; then
     echo "☑️  ${GREEN}NVM has already been installed${NC}"
 else
     echo "🚀 ${CYAN}Installing NVM${NC}"
-    curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh > /dev/null
+    curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh > /dev/null
 fi
