169547dd435cbdbf96019729664902a6b5397ea4
Irena <22574186+rensftw@users.noreply.github.com>
2022-09-15 19:12:08 +0300

    neovim(tokyonight): Fix pointy border issue

--- neovim/.config/nvim/lua/user/theme.lua
+++ neovim/.config/nvim/lua/user/theme.lua
@@ -2,6 +2,9 @@ require('tokyonight').setup({
     style = 'night',
     lualine_bold = true,
     sidebars = { 'help', 'packer' },
+    styles = {
+        floats = 'normal'
+    }
 })
 
 vim.cmd [[colorscheme tokyonight]]
