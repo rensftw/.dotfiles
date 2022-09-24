c955942dfa50a823cdf838e16666422ed9fe29b6
Irena <22574186+rensftw@users.noreply.github.com>
2022-09-24 19:24:14 +0300

    nnn: Add bookmarks

--- nnn/.config/nnn/.nnnrc.zsh
+++ nnn/.config/nnn/.nnnrc.zsh
@@ -5,5 +5,6 @@
 # -J: disable auto-jump on selection (eg. selecting an entry will no longer move cursor to the next entry)
 # -r: show cp, mv progress
 export NNN_OPTS='AedHJr'
-export NNN_PLUG='p:preview-tui;o:fzopen;d:diffs;m:nmount;u:getplugs'
+export NNN_PLUG='p:preview-tui;o:fzopen;d:diffs;m:nmount;v:vmount;u:getplugs'
+export NNN_BMS="v:/Volumes;b:/Volumes/Backup;d:$HOME/.dotfiles"
 export NNN_FIFO='/tmp/nnn.fifo'
