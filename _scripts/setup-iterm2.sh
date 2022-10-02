50654deb9a5a9f7fadd7529f1291ecd6f8f6f09c
Irena <22574186+rensftw@users.noreply.github.com>
2022-10-02 23:11:35 +0300

    iterm: Remove iTerm2

--- _scripts/setup-iterm2.sh
+++
@@ -1,6 +0,0 @@
-INSTRUCTIONS="
-💻 ${CYAN}Complete the iTerm2 setup by importing ${PURPLE}.dotfiles/_iterm2/com.googlecode.iterm2.plist${NC} in:
-Preferences > General > Preferences > Load preferences from a custom folder or URL${NC}
-
-"
-printf "$INSTRUCTIONS"
