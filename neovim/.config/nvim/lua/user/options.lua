04a4519c4fcbaa34e6a4bf8ef811db0ee33eb0f4
Irena <22574186+rensftw@users.noreply.github.com>
2022-10-02 13:35:31 +0300

    neovim: Remove redundant true color configs

--- neovim/.config/nvim/lua/user/options.lua
+++ neovim/.config/nvim/lua/user/options.lua
@@ -68,8 +68,6 @@ vim.o.diffopt = vim.o.diffopt .. 'filler' -- show filler for deleted lines
 
 -- Enable true colors, if possible
 vim.o.termguicolors = true
-vim.g['&t_8f'] = '\\<Esc>[38;2;%lu;%lu;%lum'
-vim.g['&t_8b'] = '\\<Esc>[48;2;%lu;%lu;%lum'
 
 -- Search settings
 vim.o.path = vim.o.path .. '**' -- search upwards and downwards the directory
