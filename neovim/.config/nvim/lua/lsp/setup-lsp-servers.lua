e256e35bf2cd8112612770b6a1a6ac5d014f6fda
Irena <22574186+rensftw@users.noreply.github.com>
2022-11-22 23:39:39 +0200

    neovim(prettier): Use prettier via null-ls

--- neovim/.config/nvim/lua/lsp/setup-lsp-servers.lua
+++ neovim/.config/nvim/lua/lsp/setup-lsp-servers.lua
@@ -68,8 +68,12 @@ for _, lsp in ipairs(servers) do
     }
 end
 
-require('null-ls').setup({
+local null_ls = require('null-ls')
+null_ls.setup({
     sources = {
-        require('null-ls').builtins.diagnostics.vale,
+        null_ls.builtins.diagnostics.vale,
+        -- null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.formatting.prettier,
     },
 })
+
