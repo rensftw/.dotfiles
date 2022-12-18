a38991309c5b45d240fbf0e8c372b43181f22898
Irena <22574186+rensftw@users.noreply.github.com>
2022-12-19 00:47:28 +0200

    neovim(treesitter): Add treesitter objects

--- neovim/.config/nvim/lua/plugin/nvim-treesitter-rc.lua
+++ neovim/.config/nvim/lua/plugin/nvim-treesitter-rc.lua
@@ -36,4 +36,57 @@ require 'nvim-treesitter.configs'.setup {
     },
     highlight = { enable = true },
     indent = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<c-a>',
+            node_incremental = '<c-a>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<c-d>',
+        },
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['aa'] = '@parameter.outer',
+                ['ia'] = '@parameter.inner',
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                [']f'] = '@function.outer',
+                [']c'] = '@class.outer',
+            },
+            goto_next_end = {
+                [']F'] = '@function.outer',
+                [']C'] = '@class.outer',
+            },
+            goto_previous_start = {
+                ['[f'] = '@function.outer',
+                ['[c'] = '@class.outer',
+            },
+            goto_previous_end = {
+                ['[F'] = '@function.outer',
+                ['[C'] = '@class.outer',
+            },
+        },
+        -- swap = {
+        --     enable = true,
+        --     swap_next = {
+        --         ['<leader>s'] = '@parameter.inner',
+        --     },
+        --     swap_previous = {
+        --         ['<leader>S'] = '@parameter.inner',
+        --     },
+        -- },
+    }
 }
