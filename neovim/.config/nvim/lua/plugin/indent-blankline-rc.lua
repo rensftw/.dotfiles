e2fbfff17224903f8309bddee9611246b1d50136
Irena <22574186+rensftw@users.noreply.github.com>
2022-12-23 22:11:13 +0200

    neovim(indent-blankline): Stop adding indentation to prompts, quickfix, help, man, lspinfo, checkhealth, etc

--- neovim/.config/nvim/lua/plugin/indent-blankline-rc.lua
+++ neovim/.config/nvim/lua/plugin/indent-blankline-rc.lua
@@ -1,34 +1,20 @@
---[[
--- Each treesitter grammar definers their own names for things.
--- More info: https://github.com/lukas-reineke/indent-blankline.nvim/issues/271
--- The following patterns cover JS/TS
---]]
-vim.g.indent_blankline_context_patterns = {
-    'class',
-    'return',
-    'function',
-    'method',
-    '^if',
-    '^else',
-    '^while',
-    '^for',
-    '^object',
-    '^table',
-    'block',
-    'arguments',
-    'jsx_element',
-    'jsx_self_closing_element',
-    'try_statement',
-    'catch_clause',
-    'import_statement',
-    'operation_type'
-}
-
-vim.g.indent_blankline_buftype_exclude = { 'terminal' }
-vim.g.indent_blankline_filetype_exclude = { 'alpha', 'help', 'mason' }
-
 require('indent_blankline').setup {
-    space_char_blankline = " ",
+    indent_blankline_buftype_exclude = {
+        'terminal',
+        'nofile',
+        'quickfix',
+        'prompt',
+    },
+    indent_blankline_filetype_exclude = {
+        'checkhealth',
+        'lspinfo',
+        'help',
+        'man',
+        'packer',
+        'mason',
+        'alpha',
+    },
+    space_char_blankline = ' ',
     show_current_context = true,
     show_current_context_start = true,
     use_treesitter = true,
