d6117b86bed89254019686059071348bb5cf7c3e
Irena <22574186+rensftw@users.noreply.github.com>
2023-04-25 22:11:39 +0300

    scripts: Improve logic in stow/unstow scripts

--- _scripts/stow.sh
+++ _scripts/stow.sh
@@ -1,17 +1,33 @@
+#!/usr/bin/env bash
+
 echo "🧹 ${CYAN}Clean default zsh artifacts${NC}"
-rm -rf $HOME/.zshrc $HOME/.zshenv $HOME/.zsh $HOME/.zsh_plugins &> /dev/null
+rm -rf "$HOME"/.zshrc "$HOME"/.zshenv "$HOME"/.zsh "$HOME"/.zsh_plugins &> /dev/null
 
 # Helper directories begin with an underscore (e.g. _scripts)
-HELPER_DIR_PREFIX='_*'
+HELPER_DIR_PREFIX='_'
 
 # Store all directory names in an array
-DIRECTORIES=($(ls -d */))
+# `IFS=`:               sets the internal field separator to an empty string, so that spaces in directory names are preserved.
+# `read -r`:            reads a line of input and disables backslash interpretation.
+# `-d ''`:              sets the delimiter to the null character, so that directory names with spaces are correctly handled.
+# `dir`:                the variable that will hold the directory name.
+# `find .`:             searches the current directory and its subdirectories.
+# `-maxdepth 1`:        limits the search to the current directory only.
+# `-type d`:            searches for directories only.
+# `-not -path '*/\.*'`: excludes hidden directories.
+# `-not -name '.'`:     excludes the current directory.
+# `-print0`:            prints the directory names separated by null characters.
+DIRECTORIES=()
+while IFS= read -r -d '' dir; do
+    STRIPPED_DIRECTORY_NAME=$(basename "$dir")
+    DIRECTORIES+=("$STRIPPED_DIRECTORY_NAME")
+done < <(find . -maxdepth 1 -type d -not -path '*/\.*' -not -name '.' -print0)
 
 for dir in "${DIRECTORIES[@]}"; do
     # Ignore helper directories when stowing
-    if [[ $dir != $HELPER_DIR_PREFIX ]]; then
+    if ! [[ "$dir" =~ ^$HELPER_DIR_PREFIX ]]; then
         echo "🔗 Linking ${PURPLE}${dir%/}${NC}"
-        stow -vt ~ $dir
+        stow -vt ~ "$dir"
     fi
 done
 
