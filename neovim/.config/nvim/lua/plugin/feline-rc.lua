56fbe7ab5582ab3fd82071a3c39c76d1e60def03
Irena <22574186+rensftw@users.noreply.github.com>
2023-05-04 14:36:22 +0300

    neovim(feline): Update hook icon for harpoon

--- neovim/.config/nvim/lua/plugin/feline-rc.lua
+++ neovim/.config/nvim/lua/plugin/feline-rc.lua
@@ -229,7 +229,7 @@ local c = {
 		provider = function()
             local harpoon_number = require('harpoon.mark').get_index_of(vim.fn.bufname())
             if harpoon_number then
-                return 'ﯠ ' .. harpoon_number
+                return '󰛢 ' .. harpoon_number
             else
                 return ''
             end
