30fd665322f341a7c75393ed2561a3a2a19f11cb
Irena <22574186+rensftw@users.noreply.github.com>
2023-05-05 12:28:58 +0300

    neovim(nvim-tree): Add mapping for showing node info

--- neovim/.config/nvim/lua/plugin/nvim-tree-rc.lua
+++ neovim/.config/nvim/lua/plugin/nvim-tree-rc.lua
@@ -7,6 +7,7 @@ local function on_attach(bufnr)
 
     vim.keymap.set('n', '<C-v>', api.node.open.vertical,         opts('Open: Vertical Split'))
     vim.keymap.set('n', '<C-x>', api.node.open.horizontal,       opts('Open: Horizontal Split'))
+    vim.keymap.set('n', '<C-k>', api.node.show_info_popup,       opts('Info'))
     vim.keymap.set('n', '<CR>',  api.node.open.edit,             opts('Open'))
     vim.keymap.set('n', 'l',     api.node.open.edit,             opts('Open'))
     vim.keymap.set('n', '<BS>',  api.node.navigate.parent_close, opts('Close Directory'))
