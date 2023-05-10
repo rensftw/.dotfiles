432f9dafe3520ab92e1d8fe8f5075c9a2db79fc1
Irena <22574186+rensftw@users.noreply.github.com>
2023-05-10 22:43:01 +0300

    neovim(chatgpt): Add new keybindings

--- neovim/.config/nvim/lua/plugin/chatgpt-rc.lua
+++ neovim/.config/nvim/lua/plugin/chatgpt-rc.lua
@@ -1,9 +1,14 @@
 require('chatgpt').setup({
+    popup_input = {
+        prompt = '  ',
+        submit = '<C-Enter>',
+        submit_n = '<C-Enter>',
+    },
     chat = {
         question_sign = '',
         answer_sign = '❯',
         keymaps = {
-            close = { '<C-c>', 'q' },
+            close = { '<C-c>', },
             yank_last = '<C-y>',
             yank_last_code = '<C-k>',
             scroll_up = '<C-u>',
@@ -11,9 +16,20 @@ require('chatgpt').setup({
             toggle_settings = '<C-o>',
             new_session = '<C-n>',
             cycle_windows = '<Tab>',
+            cycle_modes = '<C-v>', -- opens ChatGPT to the right
             select_session = { '<CR>', '<Space>' },
             rename_session = { 'cw', 'r' },
             delete_session = 'dd',
+            draft_message = '<C-m>',
+            toggle_message_role = '<C-r>',
+            toggle_system_role_open = '<C-s>',
+        },
+    },
+    system_window = {
+        border = {
+            text = {
+                top = ' 󱓥 ',
+            },
         },
     },
 })
