04e19908e60408090a35985f84fde8e2b63087c3
Irena <22574186+rensftw@users.noreply.github.com>
2023-08-17 22:35:06 +0300

    neovim(nnn.nvim): Enable built-in previews and fix copy_to_clipboard keybinding, so that fzcd and fzopen work again

--- neovim/.config/nvim/lua/plugin/nnn-rc.lua
+++ neovim/.config/nvim/lua/plugin/nnn-rc.lua
@@ -4,15 +4,21 @@ require('nnn').setup({
         style = {
             border = 'rounded'
         },
-        fullscreen = false, -- whether to fullscreen picker window when current tab is empty
+        -- -a: auto-setup temporary NNN_FIFO (described in ENVIRONMENT section)
+        -- -d: detail mode
+        -- -H: show hidden files
+        -- -P: specify plugin key to run
+        cmd = 'tmux new-session nnn -a -d -H -P=preview-tui',
+        session = 'shared',
+        -- whether to fullscreen picker window when current tab is empty
+        fullscreen = false,
     },
     mappings = {
         { '<C-t>', builtin.open_in_tab },
         { '<C-x>', builtin.open_in_split },
         { '<C-v>', builtin.open_in_vsplit },
-        { '<C-p>', builtin.open_in_preview },
-        { 'y',     builtin.copy_to_clipboard },
-        { '<C-w>', builtin.cd_to_path },
+        { '<C-y>', builtin.copy_to_clipboard },
+        { '<C-p>', builtin.cd_to_path },
         { '<C-e>', builtin.populate_cmdline },
     }
 })
