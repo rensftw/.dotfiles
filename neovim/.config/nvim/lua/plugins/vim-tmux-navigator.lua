1ce51b530b81a36eb7e2bf336a5939865251c2b8
Irena <22574186+rensftw@users.noreply.github.com>
2023-09-08 16:03:21 +0300

    neovim(lazy): Tweaks

--- neovim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ neovim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,5 +1,6 @@
 return {
     'christoomey/vim-tmux-navigator',
+    lazy = false,
     config = function()
         -- Write all buffers before navigating from Vim to tmux pane
         vim.g.tmux_navigator_save_on_switch = 2
