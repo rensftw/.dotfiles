6d3119e96bedf303ca117d1921bd3537a9c8f0bf
Irena <22574186+rensftw@users.noreply.github.com>
2023-09-11 22:11:49 +0300

    neovim(dap): Remove broken keymap and format

--- neovim/.config/nvim/lua/dap/dap-ui.lua
+++ neovim/.config/nvim/lua/dap/dap-ui.lua
@@ -44,8 +44,8 @@ return {
                 },
             },
             floating = {
-                max_height = nil, -- These can be integers or a float between 0 and 1.
-                max_width = nil, -- Floats will be treated as percentage of your screen.
+                max_height = nil,  -- These can be integers or a float between 0 and 1.
+                max_width = nil,   -- Floats will be treated as percentage of your screen.
                 border = 'single', -- Border style. Can be 'single', 'double' or 'rounded'
                 mappings = {
                     close = { 'q', '<Esc>' },
