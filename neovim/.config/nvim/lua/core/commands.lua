02d0a7286494170540bcc0e4cc134c6e138cd6bf
Irena <22574186+rensftw@users.noreply.github.com>
2023-09-11 22:09:33 +0300

    neovim(core): Add new autocommands and organize core

--- neovim/.config/nvim/lua/core/commands.lua
+++ neovim/.config/nvim/lua/core/commands.lua
@@ -3,3 +3,21 @@
 -- and return focus to the same spot it was initially
 vim.api.nvim_create_user_command('BufOnly', 'wa | %bdelete | edit # | bdelete # | normal `"', {})
 
+-- Auto-resize splits when Vim gets resized.
+vim.api.nvim_command [[autocmd VimResized * wincmd =]]
+
+-- Update a buffer's contents on focus if it changed outside of Vim.
+vim.api.nvim_command [[autocmd FocusGained,BufEnter * :checktime]]
+
+-- Unset paste on InsertLeave.
+vim.api.nvim_command [[autocmd InsertLeave * silent! set nopaste]]
+
+-- Make sure all types of requirements.txt files get syntax highlighting.
+vim.api.nvim_command [[autocmd BufNewFile,BufRead requirements*.txt set ft=python]]
+
+-- Make sure .aliases, .bash_aliases and similar files get syntax highlighting.
+vim.api.nvim_command [[autocmd BufNewFile,BufRead .*aliases* set ft=sh]]
+
+-- Ensure tabs don't get converted to spaces in Makefiles.
+vim.api.nvim_command [[autocmd FileType make setlocal noexpandtab]]
+
