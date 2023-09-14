fc33d6139ea9bda6295bed746a7da939f63bb7d1
Irena <22574186+rensftw@users.noreply.github.com>
2023-09-14 21:01:07 +0300

    neovim(lsp): Add keymaps for controlling the LSPs themselves

--- neovim/.config/nvim/lua/lsp/nvim-lsp-config.lua
+++ neovim/.config/nvim/lua/lsp/nvim-lsp-config.lua
@@ -1,5 +1,12 @@
 return {
     'neovim/nvim-lspconfig',
+    keys = {
+        { mode = { 'n' }, '<leader>li',   ':LspInfo<CR>', },
+        { mode = { 'n' }, '<leader>ll',   ':LspLog<CR>', },
+        { mode = { 'n' }, '<leader>ls',   ':LspStart<CR>', },
+        { mode = { 'n' }, '<leader>lq',   ':LspStop<CR>', },
+        { mode = { 'n' }, '<leader>lr',   ':LspRestart<CR>', },
+    },
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
         -- Manage LSP dependencies
