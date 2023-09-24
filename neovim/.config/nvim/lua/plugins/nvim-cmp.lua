709cdf1ef4a44089c1bd34dbed9c20f08e69a00d
Irena <22574186+rensftw@users.noreply.github.com>
2023-09-24 20:06:25 +0300

    neovim(cmp): Chore - use single quotes

--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -18,7 +18,7 @@ return {
 
         local has_words_before = function()
             local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
         end
 
         cmp.setup({
@@ -28,7 +28,7 @@ return {
                 end,
             },
             mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
+                ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.expand_or_jumpable() then
@@ -38,9 +38,9 @@ return {
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
+                end, { 'i', 's' }),
 
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     elseif luasnip.jumpable(-1) then
@@ -48,7 +48,7 @@ return {
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
+                end, { 'i', 's' }),
                 ['<C-c>'] = cmp.mapping.close(),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
