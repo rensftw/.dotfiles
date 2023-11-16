721e1c1eca8256db130a4bdbcccb79d745d59a45
Irena <22574186+rensftw@users.noreply.github.com>
2023-11-16 19:51:52 +0200

    neovim(indent): Drop indent-blankline in favour of mini.indentscope

---
+++ neovim/.config/nvim/lua/plugins/mini-indentscope.lua
@@ -0,0 +1,32 @@
+return {
+    'echasnovski/mini.indentscope',
+    version = false,
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+        require('mini.indentscope').setup({
+            draw = {
+                animation = require('mini.indentscope').gen_animation.none(),
+            },
+            symbol = '▏',
+        })
+
+        local augroup = vim.api.nvim_create_augroup
+        local autocmd = vim.api.nvim_create_autocmd
+
+        autocmd('FileType', {
+            group = augroup('exclude_filetypes_for_mini_indentscope', {}),
+            pattern = {
+                'checkhealth',
+                'lspinfo',
+                'help',
+                'man',
+                'lazy',
+                'mason',
+                'alpha',
+            },
+            callback = function(event)
+                vim.b.miniindentscope_disable = false
+            end,
+        })
+    end,
+}
