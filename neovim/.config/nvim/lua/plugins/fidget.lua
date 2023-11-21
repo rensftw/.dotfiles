55cd1f2ee4be633646e83608de2a80b8965814a4
Irena <22574186+rensftw@users.noreply.github.com>
2023-11-21 11:22:02 +0200

    neovim(fidget): Update to new version

--- neovim/.config/nvim/lua/plugins/fidget.lua
+++ neovim/.config/nvim/lua/plugins/fidget.lua
@@ -1,23 +1,28 @@
 return {
     'j-hui/fidget.nvim',
-    tag = 'legacy',
     lazy = true,
     event = 'LspAttach',
     enabled = true,
     config = function()
-        require('fidget').setup {
-            text = {
-                spinner = 'dots', -- animation shown when tasks are ongoing
+        require('fidget').setup({
+            -- Options related to LSP progress subsystem
+            progress = {
+                ignore = {}, -- List of LSP servers to ignore
+                display = {
+                    render_limit = 16, -- How many LSP messages to show at once
+                    done_ttl = 5, -- How long a message should persist after completion
+                    done_icon = '✔', -- Icon shown when all LSP progress tasks are complete
+                    progress_icon = { pattern = 'dots', period = 1 }, -- Icon shown when LSP progress tasks are in progress
+                },
             },
-            timer = {
-                fidget_decay = 6000, -- how long to keep around empty fidget, in ms
-                task_decay = 3000,   -- how long to keep around completed task, in ms
+
+            -- Options related to notification subsystem
+            notification = {
+                window = {
+                    winblend = 0,       -- Background color opacity in the notification window
+                    border = 'rounded', -- Border around the notification window
+                },
             },
-            window = {
-                relative = 'win',   -- where to anchor, either 'win' or 'editor'
-                blend = 0,          -- &winblend for the window
-                border = 'rounded', -- style of border for the fidget window
-            },
-        }
+        })
     end
 }
