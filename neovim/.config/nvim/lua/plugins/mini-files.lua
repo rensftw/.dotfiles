0a045cd74a12fc2a34680204870a3759fc26ad41
Irena <22574186+rensftw@users.noreply.github.com>
2024-01-07 22:28:06 +0200

    neovim(mini-files): Add keybinding for <CR>

--- neovim/.config/nvim/lua/plugins/mini-files.lua
+++ neovim/.config/nvim/lua/plugins/mini-files.lua
@@ -4,6 +4,11 @@ return {
         { mode = { 'n' }, '<leader>e', ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>', desc = "Open filesystem in Miller view" }
     },
     config = function()
-        require('mini.files').setup()
+        require('mini.files').setup({
+            mappings = {
+                go_in_plus = '<CR>',
+            },
+
+        })
     end,
 }
