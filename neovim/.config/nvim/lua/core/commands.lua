19c1b1c5368fe2469b724c204b3b9d861352b10d
Irena <22574186+rensftw@users.noreply.github.com>
2024-01-13 15:03:05 +0200

    neovim: Add custom command to apply @a macro to quickfix list items

--- neovim/.config/nvim/lua/core/commands.lua
+++ neovim/.config/nvim/lua/core/commands.lua
@@ -3,6 +3,10 @@
 -- and return focus to the same spot it was initially
 vim.api.nvim_create_user_command('BufOnly', 'wa | %bdelete | edit # | bdelete # | normal `"', {})
 
+-- Apply macro recorded in @a register to quickfix list items
+-- exclamation mark in `norm!` is to ensure no custom mappings of abbreviations interfere
+vim.api.nvim_create_user_command('ApplyMacroToQuickfix', 'cdo execute "norm! @a" | update', {})
+
 -- Auto-resize splits when Vim gets resized.
 vim.api.nvim_command [[autocmd VimResized * wincmd =]]
 
