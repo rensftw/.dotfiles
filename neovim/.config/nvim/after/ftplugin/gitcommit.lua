042ddf9181c2b7c1d5b00a29bea1ab86d0bbbd8d
Irena <22574186+rensftw@users.noreply.github.com>
2024-02-10 21:57:22 +0200

    neovim(ftplugin, autocmd): Use after/ftplugin directory to set filetype-specific config instead of autocommands

---
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,6 @@
+-- Extend sensible configs to improve writing experience
+local markdown_config_path = vim.fn.stdpath('config') .. '/after/ftplugin/markdown.lua'
+dofile(markdown_config_path)
+
+-- Fun fact:
+-- `Gitcommit` filetypes default to 72 lines of textwidth (and not 80 which is the default)
---
+++ neovim/.config/nvim/after/ftplugin/pandoc.lua
@@ -0,0 +1,3 @@
+-- Extend sensible configs to improve writing experience
+local markdown_config_path = vim.fn.stdpath('config') .. '/after/ftplugin/markdown.lua'
+dofile(markdown_config_path)
