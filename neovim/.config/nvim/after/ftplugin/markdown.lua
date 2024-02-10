019a1518325e1b60c285fea107bef67a87443eaa
Irena <22574186+rensftw@users.noreply.github.com>
2024-02-10 21:57:22 +0200

    neovim(ftplugin, autocmd): Use after/ftplugin directory to set filetype-specific config instead of autocommands

---
+++ neovim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+-- Improve writing experience
+vim.opt_local.spell = true
+vim.opt_local.spelllang = 'en'
+vim.opt_local.wrap = true -- Enable line wrapping for long lines
+vim.opt_local.linebreak = true -- Do not split words for linebreak
+vim.opt_local.breakindent = true -- Indent wrapped lines
+vim.opt_local.showbreak = '⋮ ' -- Symbol to indicate wrapped line
+
+-- NOTE: Uncomment to automatically insert a new line at col 80 as you type
+-- vim.opt_local.textwidth = 80
