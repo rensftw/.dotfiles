49e03c3886c826b33af72cff9c5eec74a5a1f5a1
Irena <22574186+rensftw@users.noreply.github.com>
2024-02-11 17:47:19 +0200

    neovim(spell): Add spell dictionaries

--- neovim/.config/nvim/after/ftplugin/text.lua
+++ neovim/.config/nvim/after/ftplugin/text.lua
@@ -1,3 +1,5 @@
 -- Extend sensible configs to improve writing experience
 local markdown_config_path = vim.fn.stdpath('config') .. '/after/ftplugin/markdown.lua'
 dofile(markdown_config_path)
+
+vim.opt_local.spelllang = 'en'
