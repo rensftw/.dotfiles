192e72fc50869fcb3714d0ecc0726e306c106db0
Irena <22574186+rensftw@users.noreply.github.com>
2024-02-11 18:46:56 +0200

    neovim(gitcommit): Show colorcolumn on the textwidth line

--- neovim/.config/nvim/after/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -3,6 +3,7 @@ local markdown_config_path = vim.fn.stdpath('config') .. '/after/ftplugin/markdo
 dofile(markdown_config_path)
 
 vim.opt_local.spelllang = 'en'
+vim.opt_local.colorcolumn = '+0'
 
 -- Fun fact:
 -- `Gitcommit` filetypes default to 72 lines of textwidth (and not 80 which is the default)
