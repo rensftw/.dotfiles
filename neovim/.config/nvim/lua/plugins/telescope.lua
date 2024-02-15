8d4b9a2bca161489ec868eba09bfa45e33c766ec
Irena <22574186+rensftw@users.noreply.github.com>
2024-02-15 22:21:48 +0200

    neovim(gen, llm, telescope): Add keymaps and telescope dependency

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        'nvim-telescope/telescope-ui-select.nvim'
     },
     cmd = { 'Telescope' },
     keys = function()
@@ -146,5 +147,6 @@ return {
         }
 
         telescope.load_extension('fzf')
+        telescope.load_extension('ui-select')
     end
 }
