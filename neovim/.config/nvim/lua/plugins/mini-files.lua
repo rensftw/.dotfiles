03fcdc055f5ea28951889aade43762c2fa630b10
Irena <22574186+rensftw@users.noreply.github.com>
2024-04-13 20:30:18 +0300

    neovim(mini): Add keymaps for opening files in splits

--- neovim/.config/nvim/lua/plugins/mini-files.lua
+++ neovim/.config/nvim/lua/plugins/mini-files.lua
@@ -10,7 +10,33 @@ return {
             mappings = {
                 go_in_plus = '<CR>',
             },
+        })
 
+        local map_split = function(buf_id, lhs, direction)
+            local rhs = function()
+                -- Make new window and set it as target
+                local new_target_window
+                vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+                    vim.cmd(direction .. ' split')
+                    new_target_window = vim.api.nvim_get_current_win()
+                end)
+
+                MiniFiles.set_target_window(new_target_window)
+                MiniFiles.go_in({})
+            end
+
+            -- Adding `desc` will result into `show_help` entries
+            local desc = 'Split ' .. direction
+            vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+        end
+
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'MiniFilesBufferCreate',
+            callback = function(args)
+                local buf_id = args.data.buf_id
+                map_split(buf_id, '<C-x>', 'belowright horizontal')
+                map_split(buf_id, '<C-v>', 'belowright vertical')
+            end,
         })
     end,
 }
