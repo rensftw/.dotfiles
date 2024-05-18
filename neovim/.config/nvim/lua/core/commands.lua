e0cad52fb9808250f7e6c7524bbb9cd12b2382d4
Irena <22574186+rensftw@users.noreply.github.com>
2024-05-18 21:46:46 +0300

    neovim(boxes): Add custom command for generating AsciiBoxart

--- neovim/.config/nvim/lua/core/commands.lua
+++ neovim/.config/nvim/lua/core/commands.lua
@@ -7,3 +7,20 @@ vim.api.nvim_create_user_command('BufOnly', 'wa | %bdelete | edit # | bdelete # 
 -- exclamation mark in `norm!` is to ensure no custom mappings of abbreviations interfere
 vim.api.nvim_create_user_command('ApplyMacroToQuickfix', 'cdo execute "norm! @a" | update', {})
 
+-- Create a ASCII box art over a visually selected range.
+-- By default it uses `headline` and will look like this:
+-- /*****************************/
+-- /*  H e l l o   w o r l d !  */
+-- /*****************************/
+--
+-- Run `boxes --list` to view all available styles
+vim.api.nvim_create_user_command('AsciiBoxart', function(opts)
+  -- Get the range of visually selected lines
+  local line1 = opts.line1
+  local line2 = opts.line2
+  -- Get the design option from the argument, default to "headline" if not provided
+  local design = opts.args ~= "" and opts.args or "headline"
+
+  local cmd = string.format(":%d,%d!boxes -d %s", line1, line2, design)
+  vim.cmd(cmd)
+end, { range = true, nargs = '?' })
