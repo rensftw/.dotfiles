803482973910ff7e81095a7f0517bc7cce54efdc
Irena <22574186+rensftw@users.noreply.github.com>
2024-07-06 19:44:31 +0300

    nvim(gitsigns): Update config

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -15,12 +15,12 @@ return {
     config = function()
         require('gitsigns').setup {
             signs = {
-                add          = { hl = 'GitSignsAdd',    text = '▌', numhl = 'GitSignsAddNr',    linehl = 'GitSignsAddLn'    },
-                untracked    = { hl = 'GitSignsAdd',    text = '▌', numhl = 'GitSignsAddNr',    linehl = 'GitSignsAddLn'    },
-                change       = { hl = 'GitSignsChange', text = '▌', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-                delete       = { hl = 'GitSignsDelete', text = '▌', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-                topdelete    = { hl = 'GitSignsDelete', text = '▔', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-                changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+                add          = { text = '▌' },
+                untracked    = { text = '▌' },
+                change       = { text = '▌' },
+                delete       = { text = '▌' },
+                topdelete    = { text = '▔' },
+                changedelete = { text = '~' },
             },
             current_line_blame = false,     -- Toggle with `:Gitsigns toggle_current_line_blame`
         }
