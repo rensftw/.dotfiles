5af7d82de20cff464190784b3fcc890c391bd7f1
Irena <22574186+rensftw@users.noreply.github.com>
2024-10-17 19:55:56 +0300

    neovim(todo-comments): Update keymaps to avoid clashing with tabnext/tabprev

--- neovim/.config/nvim/lua/plugins/todo-comments.lua
+++ neovim/.config/nvim/lua/plugins/todo-comments.lua
@@ -27,11 +27,11 @@ return {
             'TEST',
         }
 
-        vim.keymap.set('n', ']t', function()
+        vim.keymap.set('n', ']c', function()
             require('todo-comments').jump_next({ keywords = keywords })
         end, { desc = 'Next semantic comment' })
 
-        vim.keymap.set('n', '[t', function()
+        vim.keymap.set('n', '[c', function()
             require('todo-comments').jump_prev({ keywords = keywords })
         end, { desc = 'Previous semantic comment' })
     end
