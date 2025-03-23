243dcf1cf220c418c11c6ba4eed9cdeb307a0075
Irena <22574186+rensftw@users.noreply.github.com>
2025-03-23 20:00:26 +0200

    brew: Clean up artifacts from various utils that are no longer used

--- _scripts/setup-dash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-printf "$CYAN%s $MAGENTA$BOLD%s$NORMAL\n\n\n" "📖 Complete Dash setup by importing"  ".dotfile/_dash/Dash.dashsync"
--- _scripts/setup-rectangle.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-printf "$CYAN%s $MAGENTA$BOLD%s$NORMAL\n" "💠 Complete Rectangle setup by importing" ".dotfiles/_rectangle/RectangleConfig.json"
