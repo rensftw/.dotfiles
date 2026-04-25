5497dc67cdb1a7fa3dccbfc8bc39e0f9764af94d
Irena <22574186+rensftw@users.noreply.github.com>
2026-04-25 14:07:03 +0300

    neovim(colorizer): Swap norcalli/nvim-colorizer for catgoose fork
    Upstream norcalli/nvim-colorizer is unmaintained. The catgoose fork is
    API-compatible, uses current Nvim APIs, and auto-defers to
    vim.lsp.document_color on Nvim 0.12+.

--- neovim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ neovim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,8 +1,16 @@
 return {
-    'norcalli/nvim-colorizer.lua',
-    lazy = true,
+    'catgoose/nvim-colorizer.lua',
     event = { 'BufReadPost', 'BufNewFile' },
     config = function()
-        require 'colorizer'.setup()
-    end
+        require('colorizer').setup({
+            user_default_options = {
+                names    = true, -- CSS named colours (red, rebeccapurple, …)
+                RRGGBBAA = true, -- #RRGGBBAA
+                rgb_fn   = true, -- rgb() / rgba()
+                hsl_fn   = true, -- hsl() / hsla()
+                css      = true, -- enable the broad CSS grammar
+                css_fn   = true, -- enable CSS function forms
+            },
+        })
+    end,
 }
