680f41ea02ab9f079e66eea38897467ac6e0a84a
Irena <22574186+rensftw@users.noreply.github.com>
2026-06-15 10:48:59 +0300

    neovim(markdown-preview): Switch to a maintained markdown-preview plugin that also has better Mermaid support

--- neovim/.config/nvim/lua/plugins/markdown-preview.lua
+++ neovim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,20 +1,26 @@
 return {
-    'iamcco/markdown-preview.nvim',
+    'selimacerbas/markdown-preview.nvim',
     enabled = true,
+    dependencies = {
+        'selimacerbas/live-server.nvim',
+    },
     cmd = {
-        'MarkdownPreviewToggle',
         'MarkdownPreview',
+        'MarkdownPreviewRefresh',
         'MarkdownPreviewStop',
     },
     ft = { 'markdown' },
-    build = function() 
-        vim.fn['mkdp#util#install']() 
-    end,
-    init = function ()
-       vim.g.mkdp_auto_close = 0            -- keep preview open when switching buffers
-       vim.g.theme = 'dark'
+    config = function()
+        require('markdown_preview').setup({
+            instance_mode = 'takeover', -- 'takeover' (one tab) or 'multi' (tab per instance)
+            port = 0,                   -- 0 = auto (8421 for takeover, OS-assigned for multi)
+            open_browser = true,
+            default_theme = 'dark',
+            mermaid_renderer = 'rust,'
+        })
     end,
     keys = {
-        { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', ft = 'markdown', desc = 'Markdow preview (browser)'}
+        { '<leader>mp', '<cmd>MarkdownPreview<cr>',     ft = 'markdown', desc = 'Markdow preview (browser)' },
+        { '<leader>mP', '<cmd>MarkdownPreviewStop<cr>', ft = 'markdown', desc = 'Markdow preview stop' },
     }
 }
